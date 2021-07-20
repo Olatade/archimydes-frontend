d7ddac2def08995c255d4a2318741d5dd9776554
Olatade <4tade01@gmail.com>
2021-07-20 05:10:48 +0100

    remove unused imports from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import logo from './logo.svg';
 import './styles/nav.css';
 import UserTable from './components/UserTable';
-import Modal from 'react-modal';
-import {Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from "yup";
-import {AiOutlineExclamationCircle} from 'react-icons/ai';
-import {BiArrowBack} from 'react-icons/bi';
-import {MdClose} from 'react-icons/md';
 import CreateUser from './components/CreateUser';
 import UpdateUser from './components/UpdateUser';
 
