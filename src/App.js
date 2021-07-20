d73c12504c363f27446fd7dc1dd4eb0d757d313d
Olatade <4tade01@gmail.com>
2021-07-20 04:01:31 +0100

    install formik and yup for form validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './styles/nav.css';
-import UserTable from './components/UserTable'
+import UserTable from './components/UserTable';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    background: '#f0f0f0'
+  },
+};
 
 function App() {
   return (
@@ -13,6 +24,11 @@ function App() {
       <div className="body pt-12">
         <UserTable/>
       </div>
+      <Modal isOpen={true} style={customStyles}>
+        <div>
+          the form goes here
+        </div>
+      </Modal>
     </div>
   );
 }
