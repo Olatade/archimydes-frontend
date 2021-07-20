f3a7b7da5e9da8c5dd0cea11e8bd0d71b2f2f8b6
Olatade <4tade01@gmail.com>
2021-07-20 03:29:11 +0100

    Add table user interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/nav.css';
+import UserTable from './components/UserTable'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="border border-red-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="nav">
+        <p className="nav__brand-name" >Archimydes Challenge</p>
+      </div>
+
+      <div className="body pt-12">
+        <UserTable/>
+      </div>
     </div>
   );
 }
---
+++ src/components/UserTable.js
@@ -0,0 +1,49 @@
+import {FiChevronDown, FiPlus} from 'react-icons/fi';
+import {BiTrashAlt} from 'react-icons/bi';
+const UserTable = () =>{
+
+  return(
+    <div>
+      <div className="px-4 py-2 md:py-4 flex items-center justify-between ">
+        <h1 className="text-lg md:text-2xl font-semibold">Users</h1>
+        <button className="flex items-center rounded text-white text-sm md:text-lg bg-green-400 hover:bg-green-300 py-2 px-4 "><span className="mt-1"><FiPlus/></span><span className="pl-1" >Create User</span> </button>
+      </div>
+
+      <div className="px-3 py-2 md:py-4 flex justify-center">
+        <table className="w-full text-md bg-white shadow-md rounded mb-4">
+
+          <thead className="">
+            <tr className="border-b">
+                <th className="text-left p-3 px-5 text-xs md:text-sm font-normal">NAME</th>
+                <th className="text-left p-3 px-5 text-xs md:text-sm font-normal"><p className="flex items-center"> <span className="pr-1">EMAIL</span> <FiChevronDown/></p></th>
+                <th className="text-left p-3 px-5 text-xs md:text-sm font-normal"><p className="flex items-center"> <span className="pr-1">ROLE</span>  <FiChevronDown/></p></th>
+                <th className="text-left p-3 px-5 text-xs md:text-sm font-normal">ACTIONS</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr className="border-b cursor-pointer transition-all duration-300 hover:bg-gray-50 ">
+                <td className="p-3 px-5 text-xs md:text-sm font-bold">user name</td>
+                <td className="p-3 px-5 text-xs md:text-sm font-bold">user email</td>
+                <td className="p-3 px-5 text-xs md:text-sm font-bold">Admin</td>
+                <td className="p-3 px-5 flex items-center justify-start">
+                  <button className="mt-1"><BiTrashAlt/></button>
+                </td>
+            </tr>
+            
+            <tr className="border-b cursor-pointer transition-all duration-300 hover:bg-gray-50 ">
+                <td className="p-3 px-5 text-xs md:text-sm font-bold">user name</td>
+                <td className="p-3 px-5 text-xs md:text-sm font-bold">user email</td>
+                <td className="p-3 px-5 text-xs md:text-sm font-bold">Admin</td>
+                <td className="p-3 px-5 flex items-center justify-start">
+                  <button className="mt-1"><BiTrashAlt/></button>
+                </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default UserTable;
